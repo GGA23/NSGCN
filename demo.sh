0ea0db7a2fcf6be9344a794463256f739092101f
GGA23 <136722638+GGA23@users.noreply.github.com>
2023-07-07 22:45:37 +0800

    Create demo.sh

---
+++ demo.sh
@@ -0,0 +1,4 @@
+python train.py --dataset Computers  --lr 0.1 --weight_decay 0.0 --dropout 0.6 --ks 4
+python train.py --dataset Photo  --lr 0.05 --weight_decay 0.0 --dropout 0.6 --ks 5
+python train.py --dataset Chameleon  --lr 0.08 --weight_decay 5e-6 --dropout 0.8 --ks 3
+python train.py --dataset Squirrel  --lr 0.08 --weight_decay 0.0 --dropout 0.7 --ks 4
